a159f9f6e46cb4d5e9c5c66f52d82a5e127093be
unknown <mutassem7@gmail.com>
2025-01-20 14:16:13 +0300

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
 
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: vercel()
-});
+  
+    integrations: [tailwind()]
+  });
